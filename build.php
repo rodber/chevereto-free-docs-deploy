54fdd6d6a3088e63563192f2cb6f5ce2e00c22a7
Rodolfo Berrios <inbox@rodolfoberrios.com>
2020-07-08 20:35:40 -0400

    updated due to chevere deprecation

--- build.php
+++ build.php
@@ -2,12 +2,11 @@
 
 declare(strict_types=1);
 
-use Chevere\Components\Filesystem\FileFromString;
-use Chevere\Components\Filesystem\FilesystemFactory;
 use Chevere\Components\Writer\StreamWriterFromString;
 use DocsDeploy\MarkdownIterator;
 use DocsDeploy\Modules;
 
+use function Chevere\Components\Filesystem\getFileFromString;
 use function DocsDeploy\toModuleExport;
 
 require 'vendor/autoload.php';
@@ -21,19 +20,18 @@ if (stream_resolve_include_path($sortNavFile)) {
 $modules = new Modules($iterator, $sortNav ?? []);
 $modules->execute();
 $vuePressPath = "$docs.vuepress/";
-$filesystemFactory = new FilesystemFactory;
 foreach ([
     'nav/en.js' => $modules->nav(),
     'sidebar/en.js' => $modules->sidebar(),
 ] as $file => $module) {
-    $file = $filesystemFactory->getFileFromString($vuePressPath . $file);
+    $file = getFileFromString($vuePressPath . $file);
     if (!$file->exists()) {
         $file->create();
     }
     $file->put(toModuleExport($module));
 }
 $stylesPath = $vuePressPath . 'styles/';
-$indexProjectStyl = $filesystemFactory->getFileFromString($stylesPath . 'index-project.styl');
+$indexProjectStyl = getFileFromString($stylesPath . 'index-project.styl');
 if ($indexProjectStyl->exists()) {
     $indexStyl = new StreamWriterFromString($stylesPath . 'index.styl', 'a');
     $indexStyl->write("\n\n" . $indexProjectStyl->contents());
