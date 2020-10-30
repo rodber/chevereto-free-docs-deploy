95bf4bdd5e28bc5041da006f9efc01bc8276caf5
Rodolfo Berrios <inbox@rodolfoberrios.com>
2020-10-30 10:09:55 -0300

    updates chevere

--- build.php
+++ build.php
@@ -5,16 +5,16 @@ declare(strict_types=1);
 use DocsDeploy\MarkdownIterator;
 use DocsDeploy\Modules;
 
-use function Chevere\Components\Filesystem\dirForString;
-use function Chevere\Components\Filesystem\fileForString;
+use function Chevere\Components\Filesystem\dirForPath;
+use function Chevere\Components\Filesystem\fileForPath;
 use function Chevere\Components\Writer\streamFor;
 use function DocsDeploy\toModuleExport;
 
 require 'vendor/autoload.php';
 
 $docs = getcwd() . '/docs/';
-$sortNavFile = fileForString($docs . 'sortNav.php');
-$docsDir = dirForString($docs);
+$sortNavFile = fileForPath($docs . 'sortNav.php');
+$docsDir = dirForPath($docs);
 $iterator = new MarkdownIterator($docsDir);
 $sortNav = $sortNavFile->exists()
     ? include $sortNavFile->path()->absolute()
@@ -26,14 +26,14 @@ foreach ([
     'nav/en.js' => $modules->nav(),
     'sidebar/en.js' => $modules->sidebar(),
 ] as $file => $module) {
-    $file = fileForString($vuePressPath . $file);
+    $file = fileForPath($vuePressPath . $file);
     if (!$file->exists()) {
         $file->create();
     }
     $file->put(toModuleExport($module));
 }
 $stylesPath = $vuePressPath . 'styles/';
-$indexProjectStyl = fileForString($stylesPath . 'index-project.styl');
+$indexProjectStyl = fileForPath($stylesPath . 'index-project.styl');
 if ($indexProjectStyl->exists()) {
     $indexStyl = streamFor($stylesPath . 'index.styl', 'a');
     $indexStyl->write("\n\n" . $indexProjectStyl->contents());
